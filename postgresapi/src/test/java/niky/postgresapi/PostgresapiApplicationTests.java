88ebdc23f88c4d5038dc8686f6f99d8132019925
Nikolai Kocev <nikolai_kocev@hotmail.com>
2024-01-25 15:26:19 +0100

    Added basic springinit.io packages

---
+++ postgresapi/src/test/java/niky/postgresapi/PostgresapiApplicationTests.java
@@ -0,0 +1,13 @@
+package niky.postgresapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
